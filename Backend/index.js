68ba3f5366b60c645cf93ca0f9f073f0b5023ed1
Kelechi Emmanuel <ekelechi413@gmail.com>
2026-08-21 23:30:44 +0100

    ✅✅✅✅

--- Backend/index.js
+++ Backend/index.js
@@ -9,7 +9,7 @@ app.use(cors({
     origin: [
         "http://localhost:5173",
         "http://localhost:5174",
-        "https://your-frontend.vercel.app"
+        "https://portfolio-nine-theta-10.vercel.app"
     ],
     credentials: true
 }));
